a49ab405b122276a08988d172caa6590dca0a520
####### ####### <rplatinumedition+git@gmail.com>
2025-12-12 23:31:08 -0600

    Fix indentation and brackets (and remove commmented-out code)...
    ...in `DxLib.cpp`

--- DxLib.cpp
+++ DxLib.cpp
@@ -11,24 +11,19 @@ int DxLib_Init()
     setlocale(LC_CTYPE, "ja_JP.UTF-8");
 
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-	fprintf(stderr, "Unable to init SDL: %s\n", SDL_GetError());
-	return -1;
+    	fprintf(stderr, "Unable to init SDL: %s\n", SDL_GetError());
+    	return -1;
     }
 
-    if (!
-	(screen =
-	 SDL_SetVideoMode(480 /*(int)fmax/100 */ ,
-			  420 /*(int)fymax/100 */ , 32,
-			  SDL_SWSURFACE | SDL_DOUBLEBUF))) {
-	SDL_Quit();
-	return -1;
+    if (!(screen = SDL_SetVideoMode(480, 420, 32, SDL_SWSURFACE | SDL_DOUBLEBUF))) {
+    	SDL_Quit();
+    	return -1;
     }
 
-    SDL_WM_SetCaption("Shobon no Action (しょぼんのアクション)",
-		      NULL);
+    SDL_WM_SetCaption("Shobon no Action (しょぼんのアクション)", NULL);
     SDL_ShowCursor(SDL_DISABLE);
 
-    if(IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG)
+    if (IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG)
     {
         fprintf(stderr, "Unable to init SDL_img: %s\n", IMG_GetError());
         return -1;
@@ -36,8 +31,8 @@ int DxLib_Init()
 
     //Initialize font
     if (TTF_Init() == -1) {
-	fprintf(stderr, "Unable to init SDL_ttf: %s\n", TTF_GetError());
-	return -1;
+	   fprintf(stderr, "Unable to init SDL_ttf: %s\n", TTF_GetError());
+	   return -1;
     }
 
     //Audio Rate, Audio Format, Audio Channels, Audio Buffers
@@ -45,14 +40,14 @@ int DxLib_Init()
     if (sound && Mix_OpenAudio(22050, AUDIO_S16SYS, AUDIO_CHANNELS, 1024)) {
         fprintf(stderr, "Unable to init SDL_mixer: %s\n", Mix_GetError());
         sound = false;
-        }
+    }
     //Try to get a joystick
     joystick = SDL_JoystickOpen(0);
 
     for (int i = 0; i < SDLK_LAST; i++)
-	keysHeld[i] = false;
+	   keysHeld[i] = false;
     for (int i = 0; i < FONT_MAX; i++)
-	font[i] = NULL;
+	   font[i] = NULL;
     srand(time(NULL));
 
     return 0;
@@ -70,11 +65,11 @@ void SetFontSize(byte size)
 {
     fontsize = size;
     if (font[size] == NULL) {
-	font[size] = TTF_OpenFont("res/sazanami-gothic.ttf", size);
-	if (font[size] == NULL) {
-	    printf("Unable to load font: %s\n", TTF_GetError());
-	    exit(1);
-	}
+    	font[size] = TTF_OpenFont("res/sazanami-gothic.ttf", size);
+    	if (font[size] == NULL) {
+    	    printf("Unable to load font: %s\n", TTF_GetError());
+    	    exit(1);
+    	}
     }
 }
 
@@ -89,17 +84,17 @@ void DrawString(int a, int b, const char *x, Uint32 c)
     SDL_Color color = { static_cast<Uint8>(c >> 16), static_cast<Uint8>(c >> 8), static_cast<Uint8>(c) };
     SDL_Surface *rendered = TTF_RenderUTF8_Solid(font[fontsize], x, color);
     if (fontType == DX_FONTTYPE_EDGE) {
-	SDL_Color blk = { 0, 0, 0 };
-	SDL_Surface *shadow = TTF_RenderUTF8_Solid(font[fontsize], x, blk);
-	DrawGraphZ(a - 1, b - 1, shadow);
-	DrawGraphZ(a, b - 1, shadow);
-	DrawGraphZ(a + 1, b - 1, shadow);
-	DrawGraphZ(a - 1, b, shadow);
-	DrawGraphZ(a + 1, b, shadow);
-	DrawGraphZ(a - 1, b + 1, shadow);
-	DrawGraphZ(a, b + 1, shadow);
-	DrawGraphZ(a + 1, b + 1, shadow);
-	SDL_FreeSurface(shadow);
+    	SDL_Color blk = { 0, 0, 0 };
+    	SDL_Surface *shadow = TTF_RenderUTF8_Solid(font[fontsize], x, blk);
+    	DrawGraphZ(a - 1, b - 1, shadow);
+    	DrawGraphZ(a, b - 1, shadow);
+    	DrawGraphZ(a + 1, b - 1, shadow);
+    	DrawGraphZ(a - 1, b, shadow);
+    	DrawGraphZ(a + 1, b, shadow);
+    	DrawGraphZ(a - 1, b + 1, shadow);
+    	DrawGraphZ(a, b + 1, shadow);
+    	DrawGraphZ(a + 1, b + 1, shadow);
+    	SDL_FreeSurface(shadow);
     }
     DrawGraphZ(a, b, rendered);
     SDL_FreeSurface(rendered);
@@ -127,40 +122,41 @@ void UpdateKeys()
 {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
-	switch (event.type) {
-	case SDL_KEYDOWN:
-	    keysHeld[event.key.keysym.sym] = true;
-	    break;
-	case SDL_KEYUP:
-	    keysHeld[event.key.keysym.sym] = false;
-	    break;
-	case SDL_JOYAXISMOTION:
-	    if(event.jaxis.which == 0)
-	    {
-		if(event.jaxis.axis == JOYSTICK_XAXIS)
-		{
-		    if(event.jaxis.value < 0) keysHeld[SDLK_LEFT] = true;
-		    else if(event.jaxis.value > 0) keysHeld[SDLK_RIGHT] = true;
-		    else {
-			keysHeld[SDLK_LEFT] = false;
-			keysHeld[SDLK_RIGHT] = false;
-		    }
-		}
-		else if(event.jaxis.axis == JOYSTICK_YAXIS)
-		{
-		    if(event.jaxis.value < 0) keysHeld[SDLK_UP] = true;
-		    else if(event.jaxis.value > 0) keysHeld[SDLK_DOWN] = true;
-		    else {
-			keysHeld[SDLK_UP] = false;
-			keysHeld[SDLK_DOWN] = false;
-		    }
-		}
-	    }
-	    break;
-	case SDL_QUIT:
-	    ex = true;
-	    break;
-	}
+    	switch (event.type) {
+        	case SDL_KEYDOWN:
+        	    keysHeld[event.key.keysym.sym] = true;
+        	    break;
+        	case SDL_KEYUP:
+        	    keysHeld[event.key.keysym.sym] = false;
+        	    break;
+        	case SDL_JOYAXISMOTION:
+        	    if (event.jaxis.which == 0) {
+            		if (event.jaxis.axis == JOYSTICK_XAXIS) {
+            		    if (event.jaxis.value < 0)
+                            keysHeld[SDLK_LEFT] = true;
+            		    else if (event.jaxis.value > 0)
+                            keysHeld[SDLK_RIGHT] = true;
+            		    else {
+                			keysHeld[SDLK_LEFT] = false;
+                			keysHeld[SDLK_RIGHT] = false;
+            		    }
+            		}
+            		else if (event.jaxis.axis == JOYSTICK_YAXIS) {
+            		    if (event.jaxis.value < 0)
+                            keysHeld[SDLK_UP] = true;
+            		    else if (event.jaxis.value > 0)
+                            keysHeld[SDLK_DOWN] = true;
+            		    else {
+                			keysHeld[SDLK_UP] = false;
+                			keysHeld[SDLK_DOWN] = false;
+            		    }
+            		}
+        	    }
+        	    break;
+        	case SDL_QUIT:
+        	    ex = true;
+        	    break;
+    	}
     }
 }
 
@@ -171,7 +167,8 @@ byte ProcessMessage()
 
 byte CheckHitKey(int key)
 {
-    if(key == SDLK_z && keysHeld[SDLK_SEMICOLON]) return true;
+    if (key == SDLK_z && keysHeld[SDLK_SEMICOLON])
+        return true;
     return keysHeld[key];
 }
 
@@ -179,21 +176,17 @@ byte WaitKey()
 {
     SDL_Event event;
     while (true) {
-	while (SDL_PollEvent(&event))
-	    if (event.type == SDL_KEYDOWN)
-		return event.key.keysym.sym;
+    	while (SDL_PollEvent(&event)) {
+    	    if (event.type == SDL_KEYDOWN) {
+                return event.key.keysym.sym;
+            }
+        }
     }
 }
 
-/*Uint32 GetColor(byte r, byte g, byte b)
-{
-    return r << 8 * 3 | g << 8 * 2 | b << 8 | 0xFF;
-}*/
-
 void DrawGraphZ(int a, int b, SDL_Surface * mx)
 {
-    if(mx)
-    {
+    if (mx) {
         SDL_Rect offset;
         offset.x = a;
         offset.y = b;
@@ -203,8 +196,7 @@ void DrawGraphZ(int a, int b, SDL_Surface * mx)
 
 void DrawTurnGraphZ(int a, int b, SDL_Surface * mx)
 {
-    if(mx)
-    {
+    if (mx) {
         SDL_Rect offset;
         offset.x = a;
         offset.y = b;
@@ -219,8 +211,7 @@ void DrawTurnGraphZ(int a, int b, SDL_Surface * mx)
 
 void DrawVertTurnGraph(int a, int b, SDL_Surface * mx)
 {
-    if(mx)
-    {
+    if (mx) {
         SDL_Rect offset;
         offset.x = a - mx->w / 2;
         offset.y = b - mx->h / 2;
@@ -236,11 +227,10 @@ void DrawVertTurnGraph(int a, int b, SDL_Surface * mx)
 SDL_Surface *DerivationGraph(int srcx, int srcy, int width, int height,
 			     SDL_Surface * src)
 {
-    SDL_Surface *img =
-	SDL_CreateRGBSurface(SDL_SWSURFACE, width, height,
-			     screen->format->BitsPerPixel,
-			     src->format->Rmask, src->format->Bmask,
-			     src->format->Gmask, src->format->Amask);
+    SDL_Surface *img = SDL_CreateRGBSurface(SDL_SWSURFACE, width, height,
+                            screen->format->BitsPerPixel,
+                            src->format->Rmask, src->format->Bmask,
+                            src->format->Gmask, src->format->Amask);
 
     SDL_Rect offset;
     offset.x = srcx;
@@ -259,33 +249,44 @@ SDL_Surface *LoadGraph(const char *filename)
 {
     SDL_Surface *image = IMG_Load(filename);
 
-    if (image) return image;
+    if (image) {
+        return image;
+    }
 	fprintf(stderr, "Error: Unable to load %s: %s\n", filename, IMG_GetError());
 	exit(1);
 }
 
 void PlaySoundMem(Mix_Chunk* s, int l)
 {
-    if(sound) Mix_PlayChannel(-1, s, l);
+    if (sound) {
+        Mix_PlayChannel(-1, s, l);
+    }
 }
 
 Mix_Chunk* LoadSoundMem(const char* f)
 {
-    if(!sound) return NULL;
+    if (!sound) {
+        return NULL;
+    }
 
     Mix_Chunk* s = Mix_LoadWAV(f);
-    if(s) return s;
+    if (s) {
+        return s;
+    }
     fprintf(stderr, "Error: Unable to load sound %s: %s\n", f, Mix_GetError());
     return NULL;
 }
 
 Mix_Music* LoadMusicMem(const char* f)
 {
-    if(!sound) return NULL;
+    if (!sound) {
+        return NULL;
+    }
 
     Mix_Music* m = Mix_LoadMUS(f);
-    if(m) return m;
+    if (m) {
+        return m;
+    }
     fprintf(stderr, "Error: Unable to load music %s: %s\n", f, Mix_GetError());
     return NULL;
 }
-
